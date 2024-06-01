8ee81fbf05f816e438c9dfad97bea315e209bbb6
Prafful047 <praffulawadhiya047@gmail.com>
2024-06-01 22:22:55 +0530

    Protected Route Added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 import SignUp from './pages/Login/Signup';
 import './App.css';
+import ProtectedRoute from './pages/ProtectedRoute';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
             <Routes>
-                <Route path='/' element={<Home/>} />
+                <Route path='/' element={<ProtectedRoute><Home /></ProtectedRoute>} />
                 <Route path='/login' element={<Login/>} />
                 <Route path='/signup' element={<SignUp/>} />
 
